352b3f29055e4850cf3cb5ff8fb2314dcab312dd
jirwincline <jakobinwins@gmail.com>
2019-06-07 12:07:38 -0700

    add wordcount functions to determine most preferred language

--- js/scripts.js
+++ js/scripts.js
@@ -8,5 +8,25 @@ $(document).ready(function() {
     var q3 = $("input:radio[name=q3]:checked").val();
     var q4 = $("input:radio[name=q4]:checked").val();
     var q5 = $("input:radio[name=q5]:checked").val();
+    var allQs = q1.concat(q2,q3,q4,q5);
+    console.log(allQs);
+
+    rubyWordCount = function WordCount(allQs) {
+      return allQs.split("ruby").length - 1;
+    };
+    swiftWordCount = function WordCount(allQs) {
+      return allQs.split("swift").length - 1;
+    };
+    pythonWordCount = function WordCount(allQs) {
+      return allQs.split("python").length - 1;
+    };
+
+console.log(rubyWordCount(allQs));
+console.log(swiftWordCount(allQs));
+console.log(pythonWordCount(allQs));
+
+//     allQs.prototype.count=function(q1) {
+//     return (this.length - this.replace(new RegExp(q1,"g"), '').length) / q1.length;
+// }
   });
 });
