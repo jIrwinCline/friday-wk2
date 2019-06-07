bae8fdacdf65d7d2bcf63a359a8a758990123b66
jirwincline <jakobinwins@gmail.com>
2019-06-07 12:34:36 -0700

    add branching for multiple scenarios with if statements. But not for ties between two Languages

--- js/scripts.js
+++ js/scripts.js
@@ -11,19 +11,34 @@ $(document).ready(function() {
     var allQs = q1.concat(q2,q3,q4,q5);
     console.log(allQs);
 
-    rubyWordCount = function WordCount(allQs) {
+    var  rubyWordCount = function rWordCount(allQs) {
       return allQs.split("ruby").length - 1;
     };
-    swiftWordCount = function WordCount(allQs) {
+    var swiftWordCount = function sWordCount(allQs) {
       return allQs.split("swift").length - 1;
     };
-    pythonWordCount = function WordCount(allQs) {
+    var pythonWordCount = function pWordCount(allQs) {
       return allQs.split("python").length - 1;
     };
 
-console.log(rubyWordCount(allQs));
-console.log(swiftWordCount(allQs));
-console.log(pythonWordCount(allQs));
+    console.log(rubyWordCount(allQs));
+    console.log(swiftWordCount(allQs));
+    console.log(pythonWordCount(allQs));
+
+    if (rubyWordCount(allQs) > swiftWordCount(allQs) && rubyWordCount(allQs) > pythonWordCount(allQs)) {
+      console.log("ruby");
+      $(".ruby").show();
+    }
+    if (pythonWordCount(allQs) > swiftWordCount(allQs) && pythonWordCount(allQs) > rubyWordCount(allQs)) {
+      console.log("python");
+      $(".python").show();
+    }
+    if (swiftWordCount(allQs) > pythonWordCount(allQs) && swiftWordCount(allQs) > rubyWordCount(allQs)) {
+      console.log("swift");
+      $(".swift").show();
+    }
+
+
 
 //     allQs.prototype.count=function(q1) {
 //     return (this.length - this.replace(new RegExp(q1,"g"), '').length) / q1.length;
