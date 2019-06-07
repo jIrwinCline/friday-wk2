de4519cf4583267c3f092a3c7dc6cc6b2e8c2c89
jirwincline <jakobinwins@gmail.com>
2019-06-07 11:33:08 -0700

    debug issues with form submission

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,12 @@
 $(document).ready(function() {
-  $("#survey").submit(function(event) {
-    preventDefault(event);
+  console.log("here1");
+  $("#survey form").submit(function(event) {
+    event.preventDefault();
+    console.log("here2");
     var q1 = $("input:radio[name=q1]:checked").val();
     var q2 = $("input:radio[name=q2]:checked").val();
     var q3 = $("input:radio[name=q3]:checked").val();
     var q4 = $("input:radio[name=q4]:checked").val();
     var q5 = $("input:radio[name=q5]:checked").val();
-
   });
 });
